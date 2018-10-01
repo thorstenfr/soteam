80d92b15531893933b3a2f73d293609baed7f4e8
Thorsten Freimann <tfreimann@gmx.de>
2018-10-01 09:02:20 +0200

    Buchungen werden per ajax abgerufen

--- getbuchungen.php
+++ getbuchungen.php
@@ -1,51 +1,63 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-table {
-    width: 100%;
-    border-collapse: collapse;
-}
 
-table, td, th {
-    border: 1px solid black;
-    padding: 5px;
-}
-
-th {text-align: left;}
-</style>
 </head>
 <body>
 
+
+<table class="table">
+	<tr>
+		<th>Nick</th><th>Datum</th><th>Aufgabe</th>
+	</tr>
 <?php
 require_once("inc/config.inc.php");
-$q = intval($_GET['q']);
-echo "<h1>" .$q. "</h1>";
+if ($_GET['q'] == "*") 
+{
+	$str = "OR 1";
+}
+else {
+	$str = "";
+}
 
-	$sql = $pdo->prepare("SELECT  users.nick as wer, sae_buchung.buc_created_at as wann, sae_buchung.buc_wert as wert, sae_aufgabe.auf_beschreibung as was, sae_buchung.buc_kommentar as kommentar, sae_buchung.users_id\n"
+
+$q = intval($_GET['q']);
+
+	$sql_txt = "SELECT  users.nick as wer, sae_buchung.buc_created_at as wann, sae_buchung.buc_wert as wert, sae_aufgabe.auf_beschreibung as was, sae_buchung.buc_kommentar as kommentar, sae_buchung.users_id\n"
 
     . "FROM `sae_buchung`, users, sae_aufgabe\n"
 
     . "where sae_buchung.users_id=users.id\n"
-	. "AND sae_buchung.users_id=".$q."\n"
-    . "AND sae_buchung.sae_aufgabe_auf_id=sae_aufgabe.auf_id");
+	
+	. "AND sae_buchung.users_id=".$q." ".$str."\n"
+	
+    . "AND sae_buchung.sae_aufgabe_auf_id=sae_aufgabe.auf_id";
 	
 	
+	$sql = $pdo->prepare($sql_txt);
+	
+		
 	$result = $sql->execute();
-	echo "<table>";
+	
 	
 	while($row = $sql->fetch()) {
 		
-	echo "<tr>";
-		echo "<td>".$row['users_id']."</td>";	
+	echo "<tr>";		
 		echo "<td>".$row['wer']."</td>";
 		echo "<td>".$row['wann']."</td>";
-		echo "<td>".$row['was']."</td>";
-		echo "<td>".$row['kommentar']."</td>";	
+		echo "<td>".$row['was']."</td>";		
 	echo "</tr>";
 	}
 	
+	
+	echo "</table>";
+	
+	
+	
 ?>
-</table>
+<?php
+
+
+?>
 </body>
 </html>
--- internal.php
+++ internal.php
@@ -64,51 +64,23 @@ while($row = $statement->fetch()) {
 }
 ?>
 </table>
-<h2>Buchungen neu</h2>
+<h2>Buchungen</h2>
+<p>Wählen Sie den Teamer aus, für den die Buchungen angezeigt werden sollen.</p>
 <form>
 <select name="users" onchange="showBuchungen(this.value)">
-  <option value="">Select a person:</option>
-  <option value="1">Max Mustermann</option>
-  <option value="2">Thorsten</option>
-  <option value="3">Holger</option>
-  <option value="4">Josef</option>
+<option value="*"> - ALLE - </option>
+<?php
+$statement = $pdo->prepare("SELECT * FROM users ORDER BY id");
+$result = $statement->execute();
+$count = 1;
+while($row = $statement->fetch()) {
+	echo "<option value=\"".$row['id']."\">".$row['nick']."</option>";	
+}
+?> 
   </select>
 </form>
 <br>
-<div id="txtHint"><b>Person info will be listed here...</b></div>
-
-
-<h2>Buchungen</h2>
-<table class="table">
-<tr>
-	<th>Nick</th>
-	<th>Wann</th>
-	<th>Aufgabe</th>
-	<th>Kommentar</th>	
-</tr>
-<?php
-
-$sql = $pdo->prepare("SELECT  users.nick as wer, sae_buchung.buc_created_at as wann, sae_buchung.buc_wert as wert, sae_aufgabe.auf_beschreibung as was, sae_buchung.buc_kommentar as kommentar\n"
-
-    . "FROM `sae_buchung`, users, sae_aufgabe\n"
-
-    . "where sae_buchung.users_id=users.id \n"
-
-    . "AND sae_buchung.sae_aufgabe_auf_id=sae_aufgabe.auf_id");
-	
-	$result = $sql->execute();
-
-	while($row = $sql->fetch()) {
-	echo "<tr>";	
-		echo "<td>".$row['wer']."</td>";
-		echo "<td>".$row['wann']."</td>";
-		echo "<td>".$row['was']."</td>";
-		echo "<td>".$row['kommentar']."</td>";	
-	echo "</tr>";
-	}
-	
-?>
-</table>
+<div id="txtHint"><b>Buchungen werden hier angezeigt...</b></div>
 </div>
 
 
