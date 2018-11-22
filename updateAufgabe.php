58a02b0958b5145273b60421c8ae8a0d1e8ecbb0
Thorsten Freimann <tfreimann@gmx.de>
2018-11-22 21:40:33 +0100

    Logging

--- updateAufgabe.php
+++ updateAufgabe.php
@@ -18,7 +18,8 @@ $user = check_user();
 <body>
 
 <?php
-
+	
+	error_log("updateAufgabe gestartet!", 0);
 
 	list($aufid, $beschreibung, $taetid, $auf_daueraufgabe) = explode(":", $_GET['q']);
 	
@@ -31,11 +32,16 @@ $user = check_user();
 	}
 	
 	$sql = "UPDATE `sae_aufgabe` SET `auf_beschreibung` = '" . $beschreibung . "' WHERE `sae_aufgabe`.`auf_id` = " . $aufid . " AND sae_team_id=" . $user['sae_team_id'];
+	error_log("SQL: (" . $sql . ")", 0);/Users/thorsten/.bitnami/stackman/machines/xampp/volumes/root/htdocs/soteam
+		
 	
 	
 	
 	if ($conn->query($sql) === TRUE) {
 	    echo "Record updated successfully";
+		error_log("Record updated successfully", 0);
+		
+		
 	} else {
 	    echo "Error updating record: " . $conn->error;
 	}
@@ -44,9 +50,11 @@ $user = check_user();
 	{
 		// Bei Dueraufgaben auch die entsprechende Tätigkeit neu schreiben
 		$sql = "UPDATE `sae_taetigkeit` SET `tae_bezeichnung` = '" . $beschreibung . "' WHERE `sae_taetigkeit`.`tae_id` = " . $taetid . " AND sae_team_id=" . $user['sae_team_id'];
+		error_log("SQL2: (" . $sql . ")", 0);
 		
 		if ($conn->query($sql) === TRUE) {
 			echo "Record updated successfully";
+			error_log("2: Record updated successfully", 0);
 		} else {
 			echo "Error updating record: " . $conn->error;
 		}
